1c1abb4641cfb2e0f6d1a31c57eed267da45a103
NorhaneCh <kn_cherif@esi.dz>
2024-06-23 00:39:42 +0100

    requirements file added

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import tokenizer_from_json
 
 # Load the saved model
-model = load_model('model.h5')
-# model.save('model_new.h5')
+try:
+    model = load_model('model.h5')
+except Exception as e:
+    st.write(f"Error loading model: {e}")
 
 # Define the max length
 max_length = 2493
@@ -32,6 +34,7 @@ def preprocess_review(review, tokenizer, max_length):
 def predict_sentiment(review, model, tokenizer, max_length):
     padded_sequence = preprocess_review(review, tokenizer, max_length)
     prediction = model.predict(padded_sequence)
+    st.write(f"Prediction value : {prediction[0][0]}")
     sentiment = 'positive' if prediction >= 0.5 else 'negative'
     return sentiment
 
@@ -42,6 +45,6 @@ review = st.text_input("Enter Movie Review")
 if st.button('Predict'):
     if review:
         sentiment = predict_sentiment(review, model, tokenizer, max_length)
-        st.write(f"Predicted Sentiment: {sentiment}")
+        st.write(f"Predicted Sentiment : {sentiment}")
     else:
         st.write("Please enter a movie review to predict its sentiment.")
